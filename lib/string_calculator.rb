b959a483ea527fdd8160b548945676d4f0e3dc32
Aditya-JOSH <aditya.kolekar@joshsoftware.com>
2025-04-11 13:43:46 +0530

    Add support for newlines as delimiter

--- lib/string_calculator.rb
+++ lib/string_calculator.rb
@@ -5,6 +5,6 @@ class StringCalculator
     return 0 if numbers.empty?
     return numbers.to_i if numbers.length == 1
 
-    numbers.split(',').map(&:to_i).sum
+    numbers.split(/,|\n/).map(&:to_i).sum
   end
 end
