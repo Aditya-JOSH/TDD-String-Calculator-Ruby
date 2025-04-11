88aeba0d3539b694ceeb88656ca5af49a6dfbaab
Aditya-JOSH <aditya.kolekar@joshsoftware.com>
2025-04-11 13:48:59 +0530

    support custom delimiters

--- spec/string_calculator_spec.rb
+++ spec/string_calculator_spec.rb
@@ -24,5 +24,9 @@ RSpec.describe StringCalculator do
     it 'supports newline as a delimiter' do
       expect(StringCalculator.add("1\n2,3")).to eq(6)
     end
+
+    it 'supports custom delimiter defined at the beginning' do
+      expect(StringCalculator.add("//;\n1;2")).to eq(3)
+    end
   end
 end
